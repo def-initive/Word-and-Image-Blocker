ae73a2065462cae45a4e3c2e0af1401c06538d8c
def-initive <153235030+def-initive@users.noreply.github.com>
2023-12-09 15:42:32 -0800

    Update Word-and-Image-Blocker.js

--- Word-and-Image-Blocker.js
+++ Word-and-Image-Blocker.js
@@ -2,7 +2,7 @@
 // @name         Word and Image Blocker
 // @namespace    https://github.com/def-initive/
 // @version      1
-// @description  Block words, phrases and images associated with those phrases.
+// @description  Block words, phrases and images associated with those phrases. For the UI to add and delete words and phrases to block, please also install the following, https://greasyfork.org/en/scripts/481834-word-and-image-blocker-settings-ui
 // @author       def-initive
 // @match        *://*/*
 // @exclude      *://github.com/*
